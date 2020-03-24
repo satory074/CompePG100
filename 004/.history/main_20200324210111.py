3c6f07e030599f2173eab699c6c64a9191cb8f7c
satory074 <satory074@gmail.com>
2020-03-24 22:12:11 +0900

    add: 004

---
+++ 004/.history/main_20200324210111.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import collections
+import itertools as it
+import math
+import numpy as np
+
+# A = input()
+# A = int(input())
+# A = map(int, input().split())
+# A = list(map(int, input().split()))
+# A = [int(input()) for i in range(N)]
+#
+# c = collections.Counter()
+
+print()
---
+++ 004/.history/main_20200324214750.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import collections
+import itertools as it
+import math
+import numpy as np
+
+# A = input()
+# A = int(input())
+N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+A = [list(map(int, input().split())) for i in range(N)]
+#
+# c = collections.Counter()
+
+print(N)
