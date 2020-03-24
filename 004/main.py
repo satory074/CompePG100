205e86d800748d360540fdb7c6d2f7bdc56cfa02
satory074 <satory074@gmail.com>
2020-03-24 22:26:59 +0900

    add: 004

---
+++ 004/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import collections
+import itertools as it
+import math
+import numpy as np
+
+# A = input()
+# A = int(input())
+N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+A = [list(map(int, input().split())) for i in range(N)]
+#
+# c = collections.Counter()
+
+max_ = -1
+for m1 in range(M):
+    for m2 in range(M):
+        if m1 != m2:
+            tensu = 0
+            for n in range(N):
+                tensu += max(A[n][m1], A[n][m2])
+
+            if max_ < tensu:
+                max_ = tensu
+
+print(max_)
